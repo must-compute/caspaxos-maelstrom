80dc4009cc0686a40e96419973f794ab524d3a58
must-compute <admin@must-compute.com>
2025-03-22 10:54:25 -0400

    Add maelstrom lin-kv test scaffolding
    I grabbed these from one of my other projects:
    - message.rs: support for maelstrom msg types for testing linearizable
      key-value stores.
    - kv_store.rs: a generic simple kv store support read/write/CAS ops.
    - node.rs: for interfacing with clients and other nodes via maelstrom.

---
+++ src/message.rs
@@ -0,0 +1,142 @@
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct Message {
+    pub src: String,
+    pub dest: String,
+    pub body: BodyWithMsgId,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub struct BodyWithMsgId {
+    pub msg_id: usize,
+    #[serde(flatten)]
+    pub inner: Body,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(rename_all = "snake_case", tag = "type")]
+pub enum Body {
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk {
+        in_reply_to: usize,
+    },
+    Read {
+        key: usize, // technically it should be Any
+    },
+    ReadOk {
+        in_reply_to: usize,
+        value: usize,
+    },
+    Write {
+        key: usize, // technically it should be Any
+        value: usize,
+    },
+    WriteOk {
+        in_reply_to: usize,
+    },
+    Cas {
+        key: usize, // technically it should be Any
+        from: usize,
+        to: usize,
+    },
+    CasOk {
+        in_reply_to: usize,
+    },
+    Proxy {
+        proxied_msg: Box<Message>,
+    },
+    Error {
+        in_reply_to: usize,
+        code: ErrorCode,
+        text: String,
+    },
+}
+
+impl Body {
+    pub fn in_reply_to(&self) -> Option<usize> {
+        match self {
+            Body::ReadOk { in_reply_to, .. }
+            | Body::WriteOk { in_reply_to, .. }
+            | Body::CasOk { in_reply_to, .. }
+            | Body::Error { in_reply_to, .. } => Some(*in_reply_to),
+            Body::Init { .. }
+            | Body::InitOk { .. }
+            | Body::Read { .. }
+            | Body::Write { .. }
+            | Body::Cas { .. }
+            | Body::Proxy { .. } => None,
+        }
+    }
+    pub fn set_in_reply_to(&mut self, new_in_reply_to: usize) {
+        match self {
+            Body::InitOk {
+                ref mut in_reply_to,
+                ..
+            }
+            | Body::ReadOk {
+                ref mut in_reply_to,
+                ..
+            }
+            | Body::WriteOk {
+                ref mut in_reply_to,
+                ..
+            }
+            | Body::CasOk {
+                ref mut in_reply_to,
+                ..
+            }
+            | Body::Error {
+                ref mut in_reply_to,
+                ..
+            } => {
+                *in_reply_to = new_in_reply_to;
+            }
+            Body::Init { .. }
+            | Body::Read { .. }
+            | Body::Write { .. }
+            | Body::Cas { .. }
+            | Body::Proxy { .. } => {
+                panic!("trying to set in_reply_to on a body that doesnt have such field")
+            }
+        }
+    }
+}
+
+// https://github.com/jepsen-io/maelstrom/blob/main/doc/protocol.md#errors
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone)]
+#[repr(u8)]
+pub enum ErrorCode {
+    Timeout = 0,
+    NotSupported = 10,
+    TemporarilyUnavailable = 11,
+    MalformedRequest = 12,
+    Crash = 13,
+    Abort = 14,
+    KeyDoesNotExist = 20,
+    PreconditionFailed = 22,
+    TxnConflict = 30,
+}
+
+impl std::fmt::Display for ErrorCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ErrorCode::Timeout => write!(f, "timeout"),
+            ErrorCode::NotSupported => write!(f, "not supported"),
+            ErrorCode::TemporarilyUnavailable => write!(f, "temporarily unavailable"),
+            ErrorCode::MalformedRequest => write!(f, "malformed request"),
+            ErrorCode::Crash => write!(f, "crash"),
+            ErrorCode::Abort => write!(f, "abort"),
+            ErrorCode::KeyDoesNotExist => write!(f, "key does not exist"),
+            ErrorCode::PreconditionFailed => write!(f, "precondition failed"),
+            ErrorCode::TxnConflict => write!(f, "txn conflict"),
+        }
+    }
+}
+
+impl std::error::Error for ErrorCode {}
---
+++ src/node.rs
@@ -0,0 +1,203 @@
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, Mutex, OnceLock,
+    },
+};
+
+use rand::Rng;
+use tokio::task::yield_now;
+
+use crate::message::{Body, BodyWithMsgId, Message};
+
+pub struct MessageWithResponder {
+    msg: Message,
+    responder: Option<tokio::sync::oneshot::Sender<Message>>,
+}
+
+pub struct Node {
+    pub my_id: OnceLock<String>,
+    pub other_node_ids: OnceLock<Vec<String>>,
+    unacked: Arc<Mutex<HashMap<usize, tokio::sync::oneshot::Sender<Message>>>>,
+    pub stdout_tx: OnceLock<tokio::sync::mpsc::Sender<MessageWithResponder>>,
+    pub next_msg_id: AtomicUsize,
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Self {
+            unacked: Default::default(),
+            stdout_tx: OnceLock::new(),
+            next_msg_id: AtomicUsize::new(0),
+            my_id: OnceLock::new(),
+            other_node_ids: OnceLock::new(),
+        }
+    }
+
+    pub async fn send(
+        self: Arc<Self>,
+        dest: &str,
+        body: Body,
+        responder: Option<tokio::sync::oneshot::Sender<Message>>,
+    ) -> Message {
+        let stdout_tx = self.stdout_tx.get().unwrap();
+
+        let msg = Message {
+            src: self.my_id.get().unwrap().into(),
+            dest: dest.to_string(),
+            body: BodyWithMsgId {
+                msg_id: self.reserve_next_msg_id(),
+                inner: body,
+            },
+        };
+
+        stdout_tx
+            .send(MessageWithResponder {
+                msg: msg.clone(),
+                responder,
+            })
+            .await
+            .unwrap();
+        msg
+    }
+
+    pub async fn broadcast(
+        self: Arc<Self>,
+        body: Body,
+        responder: Option<tokio::sync::mpsc::Sender<Message>>,
+    ) {
+        let mut receiver_tasks = tokio::task::JoinSet::<Message>::new();
+
+        for destination in self.other_node_ids.get().unwrap().clone() {
+            let (tx, rx) = tokio::sync::oneshot::channel::<Message>();
+            receiver_tasks.spawn(async move {
+                rx.await
+                    .expect("should be able to recv on one of the broadcast responses")
+            });
+
+            self.clone()
+                .send(&destination, body.clone(), Some(tx))
+                .await;
+        }
+
+        tokio::spawn(async move {
+            while let Some(response_result) = receiver_tasks.join_next().await {
+                let response_message =
+                    response_result.expect("should be able to recv response during broadcast");
+                if let Some(ref responder) = responder {
+                    // The caller of broadcast might not care for all responses.
+                    // In such case, some receivers might be dropped already
+                    if !responder.is_closed() {
+                        // There is still chance for a receiver to be dropped
+                        // after the is_closed() check, so ignore the result
+                        let _ = responder.send(response_message).await;
+                    }
+                }
+            }
+        });
+    }
+
+    pub fn get_random_peer(&self) -> String {
+        let other_node_ids = self.other_node_ids.get().unwrap();
+        other_node_ids
+            .get(rand::rng().random_range(0..other_node_ids.len()))
+            .cloned()
+            .unwrap()
+    }
+
+    pub async fn run(self: Arc<Self>) -> tokio::sync::mpsc::Receiver<Message> {
+        let mut stdin_rx = self.clone().spawn_stdin_task().await;
+        self.clone().spawn_stdout_task().await;
+
+        let (tx, rx) = tokio::sync::mpsc::channel(32);
+
+        tokio::spawn(async move {
+            while let Some(msg) = stdin_rx.recv().await {
+                let mut responder: Option<tokio::sync::oneshot::Sender<Message>> = None;
+                if let Some(in_reply_to) = msg.body.inner.in_reply_to() {
+                    let mut unacked = self.unacked.lock().unwrap();
+                    responder = unacked.remove(&in_reply_to);
+                }
+
+                if let Some(responder) = responder {
+                    if !responder.is_closed() {
+                        let _ = responder.send(msg);
+                    }
+                } else {
+                    tx.send(msg).await.unwrap();
+                }
+            }
+        });
+
+        rx
+    }
+
+    async fn spawn_stdout_task(self: Arc<Self>) {
+        let (stdout_tx, mut stdout_rx) = tokio::sync::mpsc::channel::<MessageWithResponder>(32);
+
+        self.stdout_tx.set(stdout_tx).unwrap();
+
+        tokio::spawn(async move {
+            while let Some(MessageWithResponder { msg, responder }) = stdout_rx.recv().await {
+                println!(
+                    "{}",
+                    serde_json::to_string(&msg)
+                        .expect("msg being sent to STDOUT should be serializable to JSON")
+                );
+                tracing::debug!("{:?} sent {:?}", self.my_id.get(), &msg);
+
+                if let Some(responder) = responder {
+                    self.unacked
+                        .lock()
+                        .unwrap()
+                        .insert(msg.body.msg_id, responder);
+                }
+            }
+        });
+    }
+
+    async fn spawn_stdin_task(self: Arc<Self>) -> tokio::sync::mpsc::Receiver<Message> {
+        let (stdin_tx, stdin_rx) = tokio::sync::mpsc::channel::<Message>(32);
+        tokio::spawn(async move {
+            let mut input = String::new();
+            let mut is_reading_stdin = true;
+            while is_reading_stdin {
+                if let Err(e) = std::io::stdin().read_line(&mut input) {
+                    println!("readline error: {e}");
+                    is_reading_stdin = false;
+                }
+
+                let json_msg: Message = serde_json::from_str(&input)
+                    .expect(&format!("should take a JSON message. Got {:?}", input));
+                tracing::debug!("{:?} recv {:?}", self.my_id.get(), json_msg);
+
+                if let Body::Init {
+                    node_id, node_ids, ..
+                } = &json_msg.body.inner
+                {
+                    self.my_id.set(node_id.into()).unwrap();
+
+                    self.other_node_ids
+                        .set(
+                            node_ids
+                                .iter()
+                                .filter(|id| *id != node_id)
+                                .cloned()
+                                .collect(),
+                        )
+                        .unwrap();
+                }
+
+                stdin_tx.send(json_msg).await.unwrap();
+                yield_now().await;
+                input.clear();
+            }
+        });
+        stdin_rx
+    }
+
+    fn reserve_next_msg_id(&self) -> usize {
+        self.next_msg_id.fetch_add(1, Ordering::SeqCst)
+    }
+}
