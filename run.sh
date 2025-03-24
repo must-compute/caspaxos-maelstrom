de41b4a13368d95b1125858087509640db2ccc77
must-compute <admin@must-compute.com>
2025-03-24 10:39:05 -0400

    add runner script for maelstrom tests

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MAELSTROM="./maelstrom/maelstrom"
+BINARY="./target/debug/cas-paxos"
+
+if [ "$1" = "lin-kv" ]; then
+  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 20 --log-stderr --node-count 3 --concurrency 2n --rate 1 # --latency 120
+elif [ "$1" = "lin-kv-nemesis" ]; then
+  cargo build && $MAELSTROM test -w lin-kv --bin $BINARY --time-limit 15 --log-stderr --node-count 3 --concurrency 4n --rate 20 --nemesis partition --nemesis-interval 3 # --latency 120
+else
+  echo "unknown command"
+fi
