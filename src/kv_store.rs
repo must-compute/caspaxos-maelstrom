2f0a9d608d6fed6acf00d812b5ff5f7ba99f6edb
must-compute <admin@must-compute.com>
2025-03-24 10:32:26 -0400

    kv_store: impl custom ser_de for <usize,usize>

--- src/kv_store.rs
+++ src/kv_store.rs
@@ -1,10 +1,10 @@
+use serde::{ser::SerializeMap, Deserialize, Deserializer, Serialize, Serializer};
+use std::str::FromStr;
 use std::{collections::HashMap, hash::Hash};
-
-use serde::{Deserialize, Serialize};
 
 use super::message::ErrorCode;
 
-#[derive(Default, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Clone, Debug, PartialEq)]
 pub(super) struct KeyValueStore<K, V>
 where
     K: Hash + Eq + Send,
@@ -18,6 +18,10 @@ where
     K: Hash + Eq + Send,
     V: PartialEq + Send,
 {
+    pub fn new_with_inner(inner: HashMap<K, V>) -> Self {
+        Self { map: inner }
+    }
+
     pub fn is_empty(&self) -> bool {
         self.map.is_empty()
     }
@@ -43,5 +47,34 @@ where
             }
             None => Err(anyhow::Error::new(ErrorCode::KeyDoesNotExist)),
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for KeyValueStore<usize, usize> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let incoming_map: HashMap<String, usize> = HashMap::deserialize(deserializer)?;
+        let inner = incoming_map
+            .into_iter()
+            // this unwrap is fine -- this is just a toy implementation where all the clients and the nodes always use usize :)
+            .map(|(k, v)| (usize::from_str(&k).unwrap(), v))
+            .collect::<HashMap<usize, usize>>();
+
+        Ok(KeyValueStore::new_with_inner(inner))
+    }
+}
+
+impl Serialize for KeyValueStore<usize, usize> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(self.map.len()))?;
+        for (k, v) in &self.map {
+            map.serialize_entry(k, v)?;
+        }
+        map.end()
     }
 }
